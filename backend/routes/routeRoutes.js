4fc95bbce5d8518ea0d81f8b2e3baed9ad3ef59e
Jayaisuru Samarakoon <128087293+IT22569486@users.noreply.github.com>
2025-09-29 19:38:37 +0530

    feat(missings): add routeroute,routecontroller and user services

---
+++ backend/routes/routeRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const routeController = require("../controllers/routeController");
+
+// Create a new route
+router.post("/", routeController.createRoute);
+
+// Get all routes
+router.get("/", routeController.getAllRoutes);
+
+// Get a single route by ID
+router.get("/:id", routeController.getRouteById);
+
+// Update a route by ID
+router.put("/:id", routeController.updateRoute);
+
+// Delete a route by ID
+router.delete("/:id", routeController.deleteRoute);
+
+// /routes/search?start=...&destination=...
+router.get("/route/search", routeController.getRoutesByStops);
+
+// /routes/google/:id
+router.get("/google/:id", routeController.getGoogleRouteById);
+
+module.exports = router;
